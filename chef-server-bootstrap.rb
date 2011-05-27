3f7a623b577bbe4e955f1604e17a87a42f5744e3
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-05-27 23:40:54 +1000

    Changed bootstrap method to remote

--- chef-server-bootstrap.rb
+++ chef-server-bootstrap.rb
@@ -47,14 +47,7 @@ dep('chef solo bootstap') {
     false
   }
   meet {
-    shell("mkdir -p #{File.expand_path("~/")}/chef-src", :sudo => !File.writable?(File.expand_path("~/")))
-    log_shell "Downloading bootstrap version #{var(:chef_version, :default => "0.9.16")}", "wget http://s3.amazonaws.com/chef-solo/bootstrap-#{var(:chef_version)}.tar.gz -O ~/chef-src/bootstrap.tar.gz" 
-    # if var(:chef_version).to_f < 0.10
-    #   shell("touch /etc/chef/server.rb", :sudo => true)
-    #   shell("ln -s /etc/chef/server.rb /etc/chef/webui.rb", :sudo => true)
-    # end
-    log_shell "Bootstrapping Chef", "chef-solo -c /etc/chef/solo.rb -j ~/chef.json ~/chef-src/bootstrap.tar.gz", :spinner => true, :sudo => !File.writable?("/etc/chef/solo.rb")
-    
+    log_shell "Downloading and running bootstrap", "chef-solo -c /etc/chef/solo.rb -j ~/chef.json -r http://s3.amazonaws.com/chef-solo/bootstrap-#{var(:chef_version)}.tar.gz", :spinner => true, :sudo => !File.writable?("/etc/chef/solo.rb")
   }
 }
 
