93b23ef67ba5c2e87793a4294b11dd161c645141
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-05-27 18:58:12 +1000

    Added missing managed packages

--- software.rb
+++ software.rb
@@ -1,10 +1,13 @@
 dep 'core software' do
   requires {
     on :linux, 'vim.managed', 'curl.managed', 'htop.managed', 'jnettop.managed', 'screen.managed', 'nmap.managed', 'tree.managed'
-    on :osx, 'curl.managed', 'htop.managed', 'jnettop.managed', 'nmap.managed', 'tree.managed'
   }
 end
-
+dep 'nmap.managed'
+dep 'screen.managed'
+dep 'jnettop.managed' do
+  installs { via :apt, 'jnettop' }
+end
 dep 'htop.managed'
 dep 'tree.managed'
 dep 'vim.managed'
