3f5637673149b294122207421d3937dc33844142
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-05-27 19:23:27 +1000

    Added ssh deps

--- system.rb
+++ system.rb
@@ -13,3 +13,31 @@ dep 'tmp cleaning grace period', :for => :ubuntu do
   met? { !grep(/^[^#]*TMPTIME=0/, "/etc/default/rcS") }
   meet { change_line "TMPTIME=0", "TMPTIME=30", "/etc/default/rcS" }
 end
+
+dep 'secured ssh logins' do
+  requires 'sshd.managed', 'sed.managed'
+  met? {
+    # -o NumberOfPasswordPrompts=0
+    output = failable_shell('ssh -o StrictHostKeyChecking=no -o PasswordAuthentication=no nonexistentuser@localhost').stderr
+    if output.downcase['connection refused']
+      log_ok "sshd doesn't seem to be running."
+    elsif (auth_methods = output.scan(/Permission denied \((.*)\)\./).join.split(/[^a-z]+/)).empty?
+      log_error "sshd returned unexpected output."
+    else
+      (auth_methods == %w[publickey]).tap {|result|
+        log "sshd #{'only ' if result}accepts #{auth_methods.to_list} logins.", :as => (result ? :ok : :error)
+      }
+    end
+  }
+  meet {
+    change_with_sed 'PasswordAuthentication',          'yes', 'no', ssh_conf_path(:sshd)
+    change_with_sed 'ChallengeResponseAuthentication', 'yes', 'no', ssh_conf_path(:sshd)
+  }
+  after { sudo "/etc/init.d/ssh restart" }
+end
+
+dep 'lax host key checking' do
+  requires 'sed.managed'
+  met? { grep /^StrictHostKeyChecking[ \t]+no/, ssh_conf_path(:ssh) }
+  meet { change_with_sed 'StrictHostKeyChecking', 'yes', 'no', ssh_conf_path(:ssh) }
+end
