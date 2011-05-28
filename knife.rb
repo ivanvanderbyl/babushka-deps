69d13e57fee7139d693eca6e0de618283d69df8b
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-05-28 16:58:30 +1000

    Updated knife config

--- knife.rb
+++ knife.rb
@@ -8,18 +8,39 @@ meta :knife do
   end
 end
 
+dep('knife client') { requires 'knife client registered.knife'}
+
+dep('knife client registered.knife') {
+  requires "knife configured.knife"
+  
+  met?{
+    shell('knife client list') and
+    shell("knife client list |grep -E '#{shell('whoami')}$'")
+  }
+  
+  meet {
+    shell("knife client create #{me} -a -c #{knife_directory}/knife.rb --no-editor -f #{knife_directory}/#{me}.pem")
+  }
+}
+
 dep('knife configured.knife'){
   requires [
     'chef server keys.knife'
   ]
   
   met?{
-    shell('knife client list') and
-    shell("knife client list |grep -E '#{shell('whoami')}$'")
+    File.exists?(knife_directory / 'solo.rb')
   }
   meet {
     render_erb 'chef/knife.rb.erb', :to => knife_directory / 'solo.rb', :perms => '755', :sudo => false
-    shell("knife client create #{me} -a -c #{knife_directory}/knife.rb --no-editor -f #{knife_directory}/#{me}.pem")
+  }
+}
+
+dep('chef server keys.knife') {
+  requires ['dot chef directory.knife']
+  
+  meet {
+    shell("cp /etc/chef/validation.pem /etc/chef/webui.pem #{knife_directory}", :sudo => true)
   }
 }
 
@@ -32,13 +53,5 @@ dep('dot chef directory.knife') {
   meet {
     shell("mkdir -p #{knife_directory}")
     shell("chown -R $USER #{knife_directory}", :sudo => true)
-  }
-}
-
-dep('chef server keys.knife') {
-  requires ['dot chef directory.knife']
-  
-  meet {
-    shell("cp /etc/chef/validation.pem /etc/chef/webui.pem #{knife_directory}", :sudo => true)
   }
 }
