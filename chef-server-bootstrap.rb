99c184c2457764230390d9ea58947dc9c6c4a0ba
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-05-28 11:59:41 +1000

    Updated chef coniguration options

--- chef-server-bootstrap.rb
+++ chef-server-bootstrap.rb
@@ -57,24 +57,48 @@ dep('chef solo configuration') {
 }
 
 dep('chef bootstrap configuration') {
+  define_var :init_style,
+    :message => "Which init style would you like to use?",
+    :default => 'init',
+    :choice_descriptions => {
+      'init' => 'Uses init scripts that are included in the chef gem. Logs will be in /var/log/chef. Only usable with debian/ubuntu and red hat family distributions.',
+      'runit' => 'Uses runit to set up the service. Logs will be in /etc/sv/chef-client/log/main.',
+      'bluepill' => 'Uses bluepill to set up the service.',
+      'daemontools' => 'uses daemontools to set up the service. Logs will be in /etc/sv/chef-client/log/main.',
+      'bsd' => 'Prints a message with the chef-client command to use in rc.local.'
+    }
+  
+  define_var :web_ui_enabled,
+    :message => "Enable Chef Web UI?",
+    :default => "Y",
+    :choice_descriptions => {
+      'Y' => "Enabled",
+      'N' => "Disabled"
+    }
+    
   def chef_json_path
     File.expand_path("~/chef.json")
   end
   
+  def hostname
+    var(:hostname, :default => shell('hostname -f'))
+  end
+  
   met?{ File.exists?(chef_json_path) }
   meet {
+    json = {
+      "chef"=>{
+        "server_url"=>"http://localhost:4000", 
+        "server_fqdn"=> hostname, 
+        "webui_enabled"=> var(:web_ui_enabled).upcase == "Y",
+        "init_style"=> var(:init_style),
+        "client_interval"=>1800
+      }, 
+      "run_list"=>["recipe[chef::bootstrap_server]"]
+    }.to_json
+    
     shell("cat > '#{chef_json_path}'",
-      :input => 
-%({
-  "chef": {
-    "server_url": "http://localhost:4000",
-    "server_fqdn": "http://chef.easylodge.com.au",
-    "webui_enabled": true,
-    "init_style": "init",
-    "client_interval": 1800
-  },
-  "run_list": [ "recipe[chef::bootstrap_server]" ]
-}),
+      :input => json,
       :sudo => false
     )
   }
