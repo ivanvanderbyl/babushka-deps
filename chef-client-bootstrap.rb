a7a91405c4665d3d482a38d03600e2aa6cb62648
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-05-29 11:48:09 +1000

    Removed chef server env

--- chef-client-bootstrap.rb
+++ chef-client-bootstrap.rb
@@ -26,12 +26,24 @@ dep('chef client bootstrap configuration.chef') {
   
   define_var(:chef_server_url, :default => "http://chef.example.com:4000", :message => "What is the URL of your main chef server?")
   
+  define_var :init_style,
+    :message => "Which init style would you like to use?",
+    :default => 'init',
+    :choice_descriptions => {
+      'init' => 'Uses init scripts that are included in the chef gem. Logs will be in /var/log/chef. Only usable with debian/ubuntu and red hat family distributions.',
+      'runit' => 'Uses runit to set up the service. Logs will be in /etc/sv/chef-client/log/main.',
+      'bluepill' => 'Uses bluepill to set up the service.',
+      'daemontools' => 'uses daemontools to set up the service. Logs will be in /etc/sv/chef-client/log/main.',
+      'bsd' => 'Prints a message with the chef-client command to use in rc.local.'
+    }
+  
   met?{ File.exists?(chef_json_path) }
   meet {
     json = {
       "chef"=>{
         "server_fqdn"=> var(:chef_server_url), 
-        "client_interval"=>1800
+        "client_interval"=>1800,
+        "init_style"=> var(:init_style)
       },
       "recipes" => "chef::client"
     }.to_json
