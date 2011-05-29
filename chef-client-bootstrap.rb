cdcee4a48180ca7b309f3f807df52ce60683645b
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-05-29 12:19:15 +1000

    Added chef client on startup

--- chef-client-bootstrap.rb
+++ chef-client-bootstrap.rb
@@ -70,8 +70,8 @@ dep('chef client on startup'){
   }
   
   meet {
-    log("Copying start scripts from #{gem_root}/chef-#{var(:chef_version)}")
-    shell("cp #{gem_root}/chef-#{var(:chef_version)}/distro/debian/etc/init/chef-client.conf /etc/init/chef-client.conf", :sudo => true)
-    shell("cp #{gem_root}/chef-#{var(:chef_version)}/distro/debian/etc/init/chef-client.conf /etc/init.d/chef-client", :sudo => true)
+    log("Copying start scripts from #{Babushka::GemHelper.gem_path_for("chef")}/distro/debian/etc/")
+    shell("cp #{Babushka::GemHelper.gem_path_for("chef")}/distro/debian/etc/init/chef-client.conf /etc/init/chef-client.conf", :sudo => true)
+    shell("cp #{Babushka::GemHelper.gem_path_for("chef")}/distro/debian/etc/init/chef-client.conf /etc/init.d/chef-client", :sudo => true)
   }
 }
