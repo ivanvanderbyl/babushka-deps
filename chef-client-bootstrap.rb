3a1d5f0faaed9c712bc4dca48fe816f62e648051
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-06-19 22:16:17 +1000

    Added chef client registered

--- chef-client-bootstrap.rb
+++ chef-client-bootstrap.rb
@@ -1,10 +1,10 @@
 dep('bootstrap chef client'){
   define_var(:chef_version, :defailt => "0.10.0", :message => "What version of Chef do you want to install?")
-  
+
   setup {
     set :server_install, false
   }
-  
+
   requires [
     'system',
     'hostname',
@@ -20,12 +20,26 @@ dep('bootstrap chef client'){
   ]
 }
 
+dep('chef client registered') {
+  requires 'bootstrap chef client'
+
+  met? { false }
+  meet {
+    shell("sudo chef-client")
+    shell("rm -f /etc/chef/validation.pem")
+  }
+
+  unmeetable {
+    !File.exists?("/etc/chef/validation.pem")
+  }
+}
+
 dep('chef client bootstrap configuration.chef') {
   require "rubygems"
   require "json"
-  
+
   define_var(:chef_server_url, :default => "http://chef.example.com:4000", :message => "What is the URL of your main chef server?")
-  
+
   define_var :init_style,
     :message => "Which init style would you like to use?",
     :default => 'init',
@@ -36,18 +50,18 @@ dep('chef client bootstrap configuration.chef') {
       'daemontools' => 'uses daemontools to set up the service. Logs will be in /etc/sv/chef-client/log/main.',
       'bsd' => 'Prints a message with the chef-client command to use in rc.local.'
     }
-  
+
   met?{ File.exists?(chef_json_path) }
   meet {
     json = {
       "chef"=>{
-        "server_fqdn"=> var(:chef_server_url), 
+        "server_fqdn"=> var(:chef_server_url),
         "client_interval"=>1800,
         "init_style"=> var(:init_style)
       },
       "recipes" => "chef-client::config"
     }.to_json
-    
+
     shell("cat > '#{chef_json_path}'",
       :input => json,
       :sudo => false
@@ -68,7 +82,7 @@ dep('chef client on startup'){
     File.exists?("/etc/init/chef-client.conf") &&
     File.exists?("/etc/init.d/chef-client")
   }
-  
+
   meet {
     log("Copying start scripts from #{Babushka::GemHelper.gem_path_for("chef")}/distro/debian/etc/")
     shell("cp #{Babushka::GemHelper.gem_path_for("chef")}/distro/debian/etc/init/chef-client.conf /etc/init/chef-client.conf", :sudo => true)
