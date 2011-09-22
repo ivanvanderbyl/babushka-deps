d78189bb3a0c8fc4233e76561aa26e218eea6a02
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-09-22 13:00:54 +1000

    Added extra packages

--- bootstrap-vagrant-host.rb
+++ bootstrap-vagrant-host.rb
@@ -1,4 +1,4 @@
-[
+packages = [
   'lsof.managed',
   'iptables.managed',
   'jwhois.managed',
@@ -22,24 +22,14 @@
 end
 
 dep('bootstrap vagrant host'){
-  requires [
-    'lsof.managed',
-    'iptables.managed',
-    'jwhois.managed',
-    'whois.managed',
-    'curl.managed',
-    'wget.managed',
-    'rsync.managed',
-    'jnettop.managed',
-    'nmap.managed',
-    'traceroute.managed',
-    'ethtool.managed',
-    'iproute.managed',
-    'iputils-ping.managed',
-    'netcat-openbsd.managed',
-    'tcpdump.managed',
-    'elinks.managed',
-    'lynx.managed',
-    'bind9-host.managed'
-  ]
+  requires packages | ['git']
+
+  met? { false }
+
+  meet {}
+}
+
+dep('vagrant host dependencies.managed') {
+  installs packages | %w[build-essential]
+  provides %w[lsof iptables jwhois whois curl wget rsync jnettop nmap traceroute ethtool iproute iputils netcat tcpdump elinks lynx bind9 make gcc]
 }
