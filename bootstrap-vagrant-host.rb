a62207794f87dcc6392bf6fc124a0af245d5a1de
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-09-22 15:22:54 +1000

    Added extra packages

--- bootstrap-vagrant-host.rb
+++ bootstrap-vagrant-host.rb
@@ -10,7 +10,6 @@ packages = [
   'nmap',
   'traceroute',
   'ethtool',
-  'iputils-ping',
   'netcat-openbsd',
   'tcpdump',
   'elinks',
@@ -20,21 +19,13 @@ packages = [
   dep [package, 'managed'].join('.')
 end
 
-packages_without_binary = ['iproute'].each { |p|
+packages_without_binary = ['iproute', 'iputils-ping'].each { |p|
   dep [p, 'managed'].join('.') do
     provides []
   end
 }
 
-# dep('bootstrap vagrant host'){
-#   requires packages | ['git']
-#
-#   met? { false }
-#
-#   meet {}
-# }
-
 dep('vagrant host dependencies.managed') {
   requires (packages + packages_without_binary).map { |p| "#{p}.managed" }
-  # provides %w[lsof jwhois whois curl wget rsync jnettop nmap traceroute ethtool iproute iputils netcat tcpdump elinks lynx bind9 make gcc]
 }
+
