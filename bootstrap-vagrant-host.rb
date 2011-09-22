5c394aac935b709108d29bf484d326c82833f5a7
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-09-22 15:41:19 +1000

    Added extra packages

--- bootstrap-vagrant-host.rb
+++ bootstrap-vagrant-host.rb
@@ -18,7 +18,7 @@ packages = [
   dep [package, 'managed'].join('.')
 end
 
-packages_without_binary = ['iproute', 'iputils-ping', 'netcat-openbsd'].each { |p|
+packages_without_binary = ['iproute', 'iputils-ping', 'netcat-openbsd', 'libreadline5-dev', 'libssl-dev', 'libxml2-dev', 'libxslt1-dev', 'zlib1g-dev'].each { |p|
   dep [p, 'managed'].join('.') do
     provides []
   end
@@ -28,3 +28,18 @@ dep('vagrant host dependencies.managed') {
   requires (packages + packages_without_binary).map { |p| "#{p}.managed" }
 }
 
+dep('rvm system wide') {
+  met? {
+    shell("type rvm | head -1") == 'function'
+  }
+
+  meet {
+    shell('bash < <( curl -L -B http://rvm.beginrescueend.com/install/rvm )')
+    shell("mkdir -p /etc/profile.d", :sudo => true)
+    render_erb 'rvm/rvm.sh.erb', :to => '/etc/profile.d/rvm.sh', :perms => '755', :sudo => true
+  }
+}
+
+dep('vagrant host setup') {
+  requires ['vagrant host dependencies.managed', 'rvm system wide']
+}
