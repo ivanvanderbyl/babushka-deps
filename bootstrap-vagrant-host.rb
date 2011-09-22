18ea44ac3fab81bb22aaada071ced84862f519e5
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-09-22 12:51:36 +1000

    Added vagrant host bootstrap

--- bootstrap-vagrant-host.rb
+++ bootstrap-vagrant-host.rb
@@ -1,3 +1,26 @@
+[
+  'lsof.managed',
+  'iptables.managed',
+  'jwhois.managed',
+  'whois.managed',
+  'curl.managed',
+  'wget.managed',
+  'rsync.managed',
+  'jnettop.managed',
+  'nmap.managed',
+  'traceroute.managed',
+  'ethtool.managed',
+  'iproute.managed',
+  'iputils-ping.managed',
+  'netcat-openbsd.managed',
+  'tcpdump.managed',
+  'elinks.managed',
+  'lynx.managed',
+  'bind9-host.managed'
+].each do |package|
+  dep package
+end
+
 dep('bootstrap vagrant host'){
   requires [
     'lsof.managed',
