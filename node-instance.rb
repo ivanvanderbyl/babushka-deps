98842729e6c33f75dc2d874ebe1a39d77198b81f
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-10-20 12:13:08 +1100

    Added zlib headers.managed

--- node-instance.rb
+++ node-instance.rb
@@ -2,10 +2,10 @@ dep('node provisioned', :hostname_str, :chef_version) {
   requires [
     'system'.with(:hostname_str),
     'chef install dependencies.managed',
+    'zlib headers.managed',
     'ruby19.src',
     'rubygems',
     'rubygems with no docs',
-    'zlib headers.managed',
     'gems.chef'.with(chef_version)
   ]
 }
