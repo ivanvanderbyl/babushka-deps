8c3f8c4c9ad7ceec1c945a7968a892a4bcf4593d
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-10-20 11:26:59 +1100

    Added hostname str arg to hostname dep

--- node-instance.rb
+++ node-instance.rb
@@ -2,7 +2,7 @@ dep('node provisioned', :hostname_str, :chef_version) {
   requires [
     'system'.with(:hostname_str),
     'ruby',
-    'chef install dependencies.managed'.with(chef_version),
+    'chef install dependencies.managed',
     'rubygems',
     'rubygems with no docs',
     'gems.chef'.with(chef_version)
