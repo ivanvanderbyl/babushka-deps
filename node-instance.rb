858a7622ec618c1f7b61e73350e23545e53a871f
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-10-20 12:17:26 +1100

    Added 'vagrant host dependencies'

--- node-instance.rb
+++ node-instance.rb
@@ -2,6 +2,7 @@ dep('node provisioned', :hostname_str, :chef_version) {
   requires [
     'system'.with(:hostname_str),
     'chef install dependencies.managed',
+    'vagrant host dependencies',
     'zlib headers.managed',
     'ruby19.src',
     'rubygems',
