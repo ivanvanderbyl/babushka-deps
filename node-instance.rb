07e0e556c3c462f10968afa45f3a9358178d4bbc
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-11-10 12:17:59 +1100

    Updated deps

--- node-instance.rb
+++ node-instance.rb
@@ -1,12 +1,14 @@
 dep('node provisioned', :hostname_str, :chef_version) {
   requires [
-    'system'.with(:hostname_str),
-    'chef install dependencies.managed',
-    'vagrant host dependencies',
-    'zlib headers.managed',
-    'ruby19.src',
-    'rubygems',
-    'rubygems with no docs',
-    'gems.chef'.with(chef_version)
+    # 'system'.with(:hostname_str),
+    'hostname'.with(hostname_str),
+    'core software'
+    # 'chef install dependencies.managed',
+    # 'vagrant host dependencies',
+    # 'zlib headers.managed',
+    # 'ruby19.src',
+    # 'rubygems',
+    # 'rubygems with no docs',
+    # 'gems.chef'.with(chef_version)
   ]
 }
