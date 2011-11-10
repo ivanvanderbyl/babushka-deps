2212183e15a58c71a226cc8c03099354d12309c6
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-11-10 14:26:02 +1100

    Removed hostname

--- node-instance.rb
+++ node-instance.rb
@@ -1,7 +1,6 @@
 dep('node provisioned', :hostname_str, :chef_version) {
   requires [
     # 'system'.with(:hostname_str),
-    'hostname'.with(hostname_str),
     'core software',
     'chef install dependencies.managed',
     'vagrant host dependencies',
