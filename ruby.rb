8b0de5fdac9048379ae2397357d365b4aba0f631
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-04-17 09:12:50 +1000

    Updated ruby dep

--- ruby.rb
+++ ruby.rb
@@ -5,16 +5,22 @@ dep 'ruby trunk.src' do
   configure_args '--disable-install-doc', '--with-readline-dir=/usr'
 end
 
-dep 'ruby19.src' do
+dep 'ruby.src', :version, :patchlevel do
+  def version_group
+    version.to_s.scan(/^\d\.\d/).first
+  end
+  version.default!('1.9.3')
+  patchlevel.default!('p0')
   requires 'readline headers.managed', 'yaml headers.managed'
-  source 'ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p290.tar.gz'
-  provides 'ruby == 1.9.2p290', 'gem', 'irb'
+  source "ftp://ftp.ruby-lang.org/pub/ruby/#{version_group}/ruby-#{version}-#{patchlevel}.tar.gz"
+  provides "ruby == #{version}#{patchlevel}", 'gem', 'irb'
   configure_args '--disable-install-doc',
-    "--with-readline-dir=#{Babushka::Base.host.pkg_helper.prefix}",
-    "--with-libyaml-dir=#{Babushka::Base.host.pkg_helper.prefix}"
+    "--with-readline-dir=#{Babushka.host.pkg_helper.prefix}",
+    "--with-libyaml-dir=#{Babushka.host.pkg_helper.prefix}"
   postinstall {
     # TODO: hack for ruby bug where bin/* aren't installed when the build path
     # contains a dot-dir.
     shell "cp bin/* #{prefix / 'bin'}", :sudo => Babushka::SrcHelper.should_sudo?
   }
 end
+
