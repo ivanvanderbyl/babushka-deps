38c49d5ec2e9cd03ba2ef27867adacc40a46ffbb
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-04-17 08:46:15 +1000

    Added www user group

--- nginx.rb
+++ nginx.rb
@@ -195,3 +195,16 @@ dep 'http basic auth enabled.nginx', :nginx_prefix, :domain do
     restart_nginx
   }
 end
+
+dep 'www user and group' do
+  www_name = Babushka.host.osx? ? '_www' : 'www'
+  met? {
+    '/etc/passwd'.p.grep(/^#{www_name}\:/) and
+    '/etc/group'.p.grep(/^#{www_name}\:/)
+  }
+  meet {
+    sudo "groupadd #{www_name}"
+    sudo "useradd -g #{www_name} #{www_name} -s /bin/false"
+  }
+end
+
