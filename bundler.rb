ac3181d428a89be86b582bd977579766591d1995
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-04-17 09:46:27 +1000

    Added app bundled dep

---
+++ bundler.rb
@@ -0,0 +1,19 @@
+dep 'app bundled', :root, :env do
+  requires_when_unmet Dep('current dir:packages')
+  met? {
+    if !(root / 'Gemfile').exists?
+      log "No Gemfile - skipping bundling."
+      true
+    else
+      shell? 'bundle check', :cd => root, :log => true
+    end
+  }
+  meet {
+    install_args = %w[development test].include?(env) ? '' : "--deployment --without 'development test'"
+    unless shell("bundle install #{install_args}", :cd => root, :log => true)
+      confirm("Try a `bundle update`", :default => 'n') {
+        shell 'bundle update', :cd => root, :log => true
+      }
+    end
+  }
+end
