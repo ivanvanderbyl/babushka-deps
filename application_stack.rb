1073545290a0a52377f9dd10189f52b46d6c3245
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-04-26 12:04:25 +1000

    Added application deployable dep

---
+++ application_stack.rb
@@ -0,0 +1,8 @@
+dep('application deployable') {
+  requires  'benhoskings:user exists',
+            'benhoskings:ruby.src',
+            'testpilot:core dependencies',
+            'build essential installed',
+            'postgresql installed'
+}
+
