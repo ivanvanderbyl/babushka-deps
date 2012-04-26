88134e0bd3e9b5d5342a521b637d688faee32376
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-04-26 12:13:14 +1000

    Added application deployable dep

--- application_stack.rb
+++ application_stack.rb
@@ -1,8 +1,22 @@
 dep('application deployable') {
-  requires  'benhoskings:user exists',
+  setup {
+    unmeetable! "This dep has to be run as root." unless shell('whoami') == 'root'
+  }
+
+  requires [
+            'benhoskings:system',
             'benhoskings:ruby.src',
+            'benhoskings:user setup for provisioning',
             'testpilot:core dependencies',
-            'build essential installed',
-            'postgresql installed'
+            'testpilot:build essential installed',
+            'testpilot:postgresql installed']
 }
 
+dep('application deployed'){
+  setup {
+    unmeetable! "This dep cannot be run as root." if shell('whoami') == 'root'
+  }
+
+  requires ['ivanvanderbyl:web repo']
+
+}
