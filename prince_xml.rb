9d8e3348e4a3007c80e51ea3aed88360546291e5
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-07-01 18:14:19 +1000

    Added prince.xml

--- prince_xml.rb
+++ prince_xml.rb
@@ -1,4 +1,6 @@
 dep('prince xml installed') {
+  requires 'libgif4.managed'
+
   met? { shell? 'which prince' }
   meet {
     Babushka::Resource.get('http://www.princexml.com/download/prince_7.2-4ubuntu10.04_amd64.deb') do |download_path|
@@ -6,4 +8,8 @@ dep('prince xml installed') {
       shell "dpkg -i prince_7.2-4ubuntu10.04_amd64.deb", :sudo => true
     end
   }
+}
+
+dep('libgif4.managed') {
+  provides []
 }
