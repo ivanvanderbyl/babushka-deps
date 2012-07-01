786dbbc315fdd10627366e144f67fe011bcb98b7
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-07-01 18:17:57 +1000

    Added prince.xml

--- prince_xml.rb
+++ prince_xml.rb
@@ -3,9 +3,9 @@ dep('prince xml installed') {
 
   met? { shell? 'which prince' }
   meet {
-    Babushka::Resource.get('http://www.princexml.com/download/prince_7.2-4ubuntu10.04_amd64.deb') do |download_path|
+    Babushka::Resource.get('http://www.princexml.com/download/prince_8.1-1_ubuntu12.04_amd64.deb') do |download_path|
       shell "cp #{download_path} ./"
-      shell "dpkg -i prince_7.2-4ubuntu10.04_amd64.deb", :sudo => true
+      shell "dpkg -i prince_8.1-1_ubuntu12.04_amd64.deb", :sudo => true
     end
   }
 }
