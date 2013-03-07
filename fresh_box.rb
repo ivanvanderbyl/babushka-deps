4c5aa6a17dd072fee600fa6127aaf02003adf7a4
Ivan <ivanvanderbyl@me.com>
2013-03-07 14:03:12 -0800

    Added fresh box dep

---
+++ fresh_box.rb
@@ -0,0 +1,16 @@
+dep('secure system') {
+  requires [
+    'benhoskings:system',
+    'benhoskings:core software',
+    'fail2ban.managed',
+    'benhoskings:user setup for provisioning',
+    'benhoskings:passwordless sudo',
+    'benhoskings:secured ssh logins',
+    'user can write to usr local',
+    'benhoskings:passwordless ssh logins'
+  ]
+}
+
+dep('fail2ban.managed') {
+  installs []
+}
