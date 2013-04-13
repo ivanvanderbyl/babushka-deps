bbd2d6130cf4bf189d07673ce6cf87184162593d
Ivan <ivanvanderbyl@me.com>
2013-04-13 18:12:09 +1000

    Fixed dot file rendering

--- dot_files_setup.rb
+++ dot_files_setup.rb
@@ -1,4 +1,6 @@
 dep('dot files setup') {
+  require 'digest/sha1'
+
   def files
     Dir.glob(dependency.load_path.parent / "dot_files/dot*.erb")
   end
@@ -16,15 +18,29 @@ dep('dot files setup') {
     dot_file_path
   end
 
+  def renderable(path)
+    Babushka::Renderable.new(path)
+  end
+
+  def rendered_config(local_path)
+    renderable.send(:render_erb, dependency.load_path.parent / local_path, self)
+  end
+
   met?{
     files.all? do |file|
-      Babushka::Renderable.new(dot_file_path / local_to_remote_name(file)).from?(file)
+      path = dot_file_path / local_to_remote_name(file)
+      File.exists?(path) &&
+      Digest::SHA1.hexdigest(rendered_config(file)) == renderable(path).send(:sha_of, path)
     end
   }
 
   meet {
     files.each do |file|
-      render_erb file, :to => dot_file_path / local_to_remote_name(file), :credit => false
+      shell("cat > '#{dot_file_path / local_to_remote_name(file)}'",
+        :input => renderable(dot_file_path / local_to_remote_name(file)).send(:render_erb, dependency.load_path.parent / file, self),
+        :sudo => true
+      )
+      # render_erb file, :to => dot_file_path / local_to_remote_name(file), :credit => false
     end
   }
 }
